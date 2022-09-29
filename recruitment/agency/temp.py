5e05104b9cdb50f3df937d8a786f136c5ea6a50d
manualautomaton <emmanuel.obi@newsela.com>
2022-09-29 10:58:23 -0500

    replaces super() with duplicate implementation

--- recruitment/agency/temp.py
+++ recruitment/agency/temp.py
@@ -1,7 +1,11 @@
+import boto3
+
+from botocore.exceptions import NoRegionError
 from typing import Iterable
 from typing import Optional
 from unittest.mock import patch
 
+from recruitment.agency import Broker
 from recruitment.agency import Communicator as ActualCommunicator
 from recruitment.agency import Config
 
@@ -18,7 +22,22 @@ class Communicator(ActualCommunicator):
         self.response_provider = lambda: expected_payload or {}
         self.args_provider = lambda: expected_args or {}
         self.kwargs_provider = lambda: expected_kwargs or {}
-        super().__init__(config)
+        # below replease super() call; is copied from recruitment.agency.Communicator
+        broker = Broker(config.service_name)
+        for alias, method in broker.interface.items():
+            try:
+                client = boto3.client(
+                    service_name=config.service_name,
+                    endpoint_url=config.endpoint_url,
+                    region_name=config.region_name,
+                )
+            except (ValueError, NoRegionError) as e:
+                print('-->>>', e, e.__class__.__name__)
+                raise Communicator.FailedToInstantiate(given=config, cause=e) from e
+            except Exception as uncaught:
+                print('==>>>', uncaught, uncaught.__class__.__name__)
+                raise Communicator.FailedToInstantiate(given=config, cause=uncaught) from uncaught
+            setattr(self, alias, getattr(client, method))
 
     def __enter__(self):
         self.patcher = patch('botocore.client.BaseClient._make_api_call')
