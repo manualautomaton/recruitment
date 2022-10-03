26025054ed7b658eaa2cf67b02dc101c562463cd
manualautomaton <emmanuel.obi@newsela.com>
2022-10-03 08:08:41 -0500

    revisits session_var specificiation in CoreSession instantiation

--- recruitment/agency/__init__.py
+++ recruitment/agency/__init__.py
@@ -110,7 +110,9 @@ class Communicator:
         for alias, method in broker.interface.items():
             print('\nREGION_NAME -->>>', config.region_name)
             try:
-                client = CoreSession().create_client(
+                client = CoreSession(
+                    {'region': ('region', 'AWS_DEFAULT_REGION', config.region_name, None)}
+                ).create_client(
                     config.service_name,
                     region_name=config.region_name,
                     endpoint_url=config.endpoint_url,
